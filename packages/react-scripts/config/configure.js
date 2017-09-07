9d030a59c72638f17790121f5c978aab19117c32
Jon Crenshaw <jdcrensh@gmail.com>
2017-09-06 22:19:55 -0700

    Refactor configure.js to not be ES6 class

--- packages/react-scripts/config/configure.js
+++ packages/react-scripts/config/configure.js
@@ -7,43 +7,38 @@ const {
   validatePlugins,
 } = require('./configure.utils');
 
-class Configure {
-  constructor() {
-    const config = fs.existsSync(configPath) ? require(configPath) : {};
-    this.plugins = this.loadPlugins(config.plugins || []);
-  }
+const config = fs.existsSync(configPath) ? require(configPath) : {};
+validatePlugins(config.plugins);
 
-  loadPlugins(plugins) {
-    validatePlugins(plugins);
-    return plugins.map(p => {
-      let opts = {};
-      if (Array.isArray(p)) {
-        opts = p.length > 1 ? p[1] : {};
-        p = p[0];
-      }
-      const plugin = require(resolvePluginPath(p));
-      typeof plugin.init === 'function' && plugin.init(opts);
-      return plugin;
-    });
+const plugins = config.plugins.map(p => {
+  let opts = {};
+  if (Array.isArray(p)) {
+    opts = p.length > 1 ? p[1] : {};
+    p = p[0];
   }
-
-  webpack(config) {
-    return this.plugins.reduce((config, plugin) => {
-      if (typeof plugin.webpack === 'function') {
-        config = plugin.webpack(config, { paths: require('./paths') });
-      }
-      return config;
-    }, config);
+  const plugin = require(resolvePluginPath(p));
+  if (typeof plugin.init === 'function') {
+    plugin.init(opts);
   }
+  return plugin;
+});
 
-  paths(helpers, paths) {
-    return this.plugins.reduce((paths, plugin) => {
-      if (typeof plugin.paths === 'function') {
-        return plugin.paths(paths, { helpers });
-      }
-      return paths;
-    }, paths);
-  }
-}
+const webpack = config =>
+  plugins.reduce(
+    (config, plugin) =>
+      typeof plugin.webpack === 'function'
+        ? plugin.webpack(config, { paths: require('./paths') })
+        : config,
+    config
+  );
 
-module.exports = new Configure();
+const paths = (helpers, paths) =>
+  plugins.reduce(
+    (paths, plugin) =>
+      typeof plugin.paths === 'function'
+        ? plugin.paths(paths, { helpers })
+        : paths,
+    paths
+  );
+
+module.exports = { webpack, paths };
